843d97729ba976d43e85bdf417aef582e2d83fa7
dookebroyle <57194791+dookebroyle@users.noreply.github.com>
2022-02-12 14:39:16 -0700

    added self sign cert support for tls

--- src/main/java/com/doylebrooke/ecommerce/dao/StateRepository.java
+++ src/main/java/com/doylebrooke/ecommerce/dao/StateRepository.java
@@ -7,7 +7,8 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
-@CrossOrigin("http://localhost:4200")
+
+@CrossOrigin("https://localhost:4200")
 @RepositoryRestResource
 public interface StateRepository extends JpaRepository<State, Integer> {
 
